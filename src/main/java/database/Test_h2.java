e2595f5c41131489da49653b6ad6a4e58493d8cd
H. Gabriel Máculus <gabrielmaculus@gmail.com>
2025-08-29 09:52:39 -0300

    Benchmark Test_H2 (separate create, insert, select)

--- src/main/java/database/Test_h2.java
+++ src/main/java/database/Test_h2.java
@@ -1,4 +1,5 @@
 package database;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -6,28 +7,42 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /*
     Test h2 database without dependencies
-*/
+ */
 public class Test_h2 {
+
     public static void main(String[] args) {
-       long startTime = System.currentTimeMillis();
+        long startTime = System.currentTimeMillis();
+        long endTime;
         try (Connection conn = DriverManager.getConnection("jdbc:h2:mem:test")) {
-                Statement stmt = conn.createStatement();
-                stmt.execute("create table cities (id integer, name varchar(50))");
-                stmt.executeUpdate("insert into cities (id, name) values (1, \'General Alvear\');");
-                stmt.executeUpdate("insert into cities (id, name) values (2, \'Real del Padre\');");
-                stmt.executeUpdate("insert into cities (id, name) values (3, \'Villa Atuel\');");
-                conn.commit();
-                ResultSet rs = stmt.executeQuery("select * from cities;");
-                while(rs.next()) {
-                    System.out.println("id: " + rs.getString("id") + " name: " + rs.getString("name"));
-                }
+            Statement stmt = conn.createStatement();
+            stmt.execute("create table cities (id integer, name varchar(50))");
+            
+            endTime = System.currentTimeMillis();
+            System.out.println("Total execution time(create table): " + (endTime - startTime) + "ms");
+            startTime = System.currentTimeMillis();
+            
+            stmt.executeUpdate("insert into cities (id, name) values (1, \'General Alvear\');");
+            stmt.executeUpdate("insert into cities (id, name) values (2, \'Real del Padre\');");
+            stmt.executeUpdate("insert into cities (id, name) values (3, \'Villa Atuel\');");
+            conn.commit();
+            
+            endTime = System.currentTimeMillis();
+            System.out.println("Total execution time(insert into table): " + (endTime - startTime) + "ms");
+            startTime = System.currentTimeMillis();
+
+            ResultSet rs = stmt.executeQuery("select * from cities;");
+
+            endTime = System.currentTimeMillis();
+            System.out.println("Total execution time(select *): " + (endTime - startTime) + "ms");
+            while (rs.next()) {
+                System.out.println("id: " + rs.getString("id") + " name: " + rs.getString("name"));
             }
-        catch (SQLException ex) {
+        } catch (SQLException ex) {
             Logger.getLogger(Test_h2.class.getName()).log(Level.SEVERE, null, ex);
         }
-        long endTime = System.currentTimeMillis();
-       System.out.println("Total execution time: " + (endTime-startTime) + "ms");
+        
     }
 }
