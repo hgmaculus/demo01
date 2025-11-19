c891ce833263ee8431021b4dfd4dfa6cba06e946
H. Gabriel Máculus <gabrielmaculus@gmail.com>
2025-11-19 07:49:08 -0300

    add TestClientes.java

--- src/main/java/clientes/Cliente.java
+++ src/main/java/clientes/Cliente.java
@@ -7,6 +7,54 @@ public class Cliente {
     private String telefono;
     private String email;
 
+    public Cliente(int id, String nombre, String apellido, String telefono, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -16,85 +64,7 @@ public class Cliente {
         sb.append(", apellido=").append(apellido);
         sb.append(", telefono=").append(telefono);
         sb.append(", email=").append(email);
-        sb.append(", cuit=").append(cuit);
-        sb.append(", razon_social=").append(razon_social);
-        sb.append(", direccion=").append(direccion);
         sb.append('}');
         return sb.toString();
-    }
-
-    public Cliente(int id, String nombre, String apellido, String telefono, String email, String cuit, String razon_social, String direccion) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
-        this.email = email;
-        this.cuit = cuit;
-        this.razon_social = razon_social;
-        this.direccion = direccion;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCuit() {
-        return cuit;
-    }
-
-    public void setCuit(String cuit) {
-        this.cuit = cuit;
-    }
-
-    public String getRazon_social() {
-        return razon_social;
-    }
-
-    public void setRazon_social(String razon_social) {
-        this.razon_social = razon_social;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
     }
 }
